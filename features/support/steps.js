2ca94d06eb321d32773a8c4ca070c1202bdb65cd
Shawn Goertzen <sgoertzen@gmail.com>
2015-04-28 23:24:41 -0700

    Attempt to run program through Travis

--- features/support/steps.js
+++ features/support/steps.js
@@ -7,13 +7,11 @@ var myStepDefinitionsWrapper = function () {
   });
 
   this.When(/^I view the website$/, function (callback) {
-    //this.browser.url("https://localhost").call(callback);
-    callback();
+    this.browser.url("https://localhost").call(callback);
   });
 
   this.Then(/^it will ask for an encryption password$/, function (callback) {
-    var title = "hello"
-    //this.browser.getElement().and.notify(callback);
+    //this.browser.text('body').should.include.string('encryption key').and.notify(callback);
     callback();
   });
 }
