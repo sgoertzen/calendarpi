1beb4dae52745d7f61d9613efa93046a9beac92e
Shawn Goertzen <sgoertzen@gmail.com>
2015-05-06 21:34:23 -0700

    Removing unused method

--- exchangeparser.go
+++ exchangeparser.go
@@ -5,9 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/sgoertzen/html2text"
-	"golang.org/x/net/html"
 	"log"
-	"strings"
 	"time"
 )
 
@@ -154,46 +152,4 @@ func (a *Appointment) BuildDesc() string {
 	body := html2text.Textify(a.Body)
 	desc += body
 	return desc
-}
-
-func convertLinks(body string) string {
-	r := strings.NewReader(body)
-	doc, err := html.Parse(r)
-	if err != nil {
-		// ...
-	}
-	
-	var breakers = make(map[string]bool)
-	breakers["br"] = true
-	breakers["div"] = true
-	breakers["tr"] = true
-	breakers["li"] = true
-	
-	var f func(*html.Node, *bytes.Buffer)
-	f = func(n *html.Node, b *bytes.Buffer) {
-		processChildren := true
-		
-		if n.Type == html.ElementNode && n.Data == "head" {
-			return
-		} else if n.Type == html.ElementNode && n.Data == "a" && n.FirstChild != nil {
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c, b)
-			}
-			b.WriteString(fmt.Sprintf(" (link: %s)", n.Attr[0].Val))
-			processChildren = false
-		} else if n.Type == html.TextNode {
-			b.WriteString(n.Data)
-		} 
-		if processChildren {
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c, b)
-			}
-		}
-		if n.Type == html.ElementNode && breakers[n.Data] {
-			b.WriteString("\n")
-		}
-	}
-	var buffer bytes.Buffer
-	f(doc, &buffer)
-	return strings.TrimSpace(buffer.String())
 }
